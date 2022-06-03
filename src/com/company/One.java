cfb0667d10ca340b3324871c1f2373ef3357030f
anastasiasavych <99317597+anastasiasavych@users.noreply.github.com>
2022-06-03 11:47:16 +0300

    Initial commit

---
+++ src/com/company/One.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class One {
+    public static void main(String[] args) {
+        int a = 2;
+        for(a = 200; a < 1000; a = 2*a -1){
+                System.out.println(a);
+        }
+    }
+}
